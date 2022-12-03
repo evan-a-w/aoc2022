cddbf5ecb841f48d9f36c8e0758d4cb23a3a3e37
Evan Williams <evanawilliams@live.com>
2022-12-04 00:10:14 +0800

    😊

--- day1/day1.ml
+++ day1/day1.ml
@@ -4,19 +4,16 @@ let inp = In_channel.create "input/day1.txt"
 
 let calories =
   let accum (small, big) line =
-    if String.equal line "" then
-      (0, small :: big)
-    else
-      (Int.of_string line + small, big) in
-  In_channel.fold_lines inp ~init:(0, []) ~f:accum
-    |> fun (a, b) -> a :: b
+    if String.equal line "" then (0, small :: big)
+    else (Int.of_string line + small, big)
+  in
+  In_channel.fold_lines inp ~init:(0, []) ~f:accum |> fun (a, b) -> a :: b
 
 let () =
-  Printf.printf "Part 1: %d\n" (List.max_elt ~compare:Int.compare calories |> Option.value_exn);
-  Printf.printf
-    "Part 2: %d\n"
+  Printf.printf "Part 1: %d\n"
+    (List.max_elt ~compare:Int.compare calories |> Option.value_exn);
+  Printf.printf "Part 2: %d\n"
     (calories
-      |> List.sort ~compare:(Fn.flip Int.compare)
-      |> (Fn.flip List.take) 3
-      |> List.fold ~init:0 ~f:(+))
-
+    |> List.sort ~compare:(Fn.flip Int.compare)
+    |> (Fn.flip List.take) 3
+    |> List.fold ~init:0 ~f:( + ))
--- day2/day2.ml
+++ day2/day2.ml
@@ -11,45 +11,37 @@ module RPS = struct
     | "C" | "Z" -> Scissors
     | _ -> failwith "Invalid input"
 
-  let int = function
-    | Rock -> 1
-    | Paper -> 2
-    | Scissors -> 3
+  let int = function Rock -> 1 | Paper -> 2 | Scissors -> 3
 
-  let win a b = match a, b with
+  let win a b =
+    match (a, b) with
     | Scissors, Rock | Rock, Paper | Paper, Scissors -> 2
     | _ when int a = int b -> 1
     | _ -> 0
 
-  let to_win = function
-    | Rock -> Paper
-    | Paper -> Scissors
-    | Scissors -> Rock
-
-  let to_lose = function
-    | Rock -> Scissors
-    | Paper -> Rock
-    | Scissors -> Paper
-
+  let to_win = function Rock -> Paper | Paper -> Scissors | Scissors -> Rock
+  let to_lose = function Rock -> Scissors | Paper -> Rock | Scissors -> Paper
   let score_match a b = int b + (win a b * 3)
 end
 
-let matches = In_channel.input_lines inp
-  |> List.map ~f:(Fn.compose
-                   (List.map ~f:RPS.of_string)
-                   (String.split ~on:' '))
+let matches =
+  In_channel.input_lines inp
+  |> List.map ~f:(Fn.compose (List.map ~f:RPS.of_string) (String.split ~on:' '))
 
 let () =
-  Printf.printf "Part 1: %d\n" @@
-    List.sum (module Int) 
-             matches
-             ~f:(function | [a; b] -> RPS.score_match a b | _ -> failwith "fk u");
-  Printf.printf "Part 2: %d\n" @@
-    List.sum (module Int) 
-             matches
-             ~f:(function
-                  | [a; b] -> (match b with
-                    | RPS.Rock -> RPS.score_match a (RPS.to_lose a)
-                    | RPS.Paper -> RPS.score_match a a
-                    | RPS.Scissors -> RPS.score_match a (RPS.to_win a))
-                  | _ -> failwith "fk u");
+  Printf.printf "Part 1: %d\n"
+  @@ List.sum
+       (module Int)
+       matches
+       ~f:(function [ a; b ] -> RPS.score_match a b | _ -> failwith "fk u");
+  Printf.printf "Part 2: %d\n"
+  @@ List.sum
+       (module Int)
+       matches
+       ~f:(function
+         | [ a; b ] -> (
+             match b with
+             | RPS.Rock -> RPS.score_match a (RPS.to_lose a)
+             | RPS.Paper -> RPS.score_match a a
+             | RPS.Scissors -> RPS.score_match a (RPS.to_win a))
+         | _ -> failwith "fk u")
